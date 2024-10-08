919d239358ce1e7674a0c9f9cf89c45b7564814d
varrios <lucjan.gackowski@gmail.com>
2024-10-08 20:42:07 +0200

    repositories added

---
+++ src/main/java/lg/pg/aui/repository/AppointmentRepository.java
@@ -0,0 +1,13 @@
+package lg.pg.aui.repository;
+
+import lg.pg.aui.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
+    List<Appointment> findByPatientName(String appointmentDate);
+}
