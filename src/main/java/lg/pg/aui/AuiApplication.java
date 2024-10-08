f6e4c0292d4877f7504432ef46b3e925e5ac0e99
varrios <lucjan.gackowski@gmail.com>
2024-10-08 20:05:00 +0200

    Project files initialized

---
+++ src/main/java/lg/pg/aui/AuiApplication.java
@@ -0,0 +1,13 @@
+package lg.pg.aui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuiApplication.class, args);
+	}
+
+	}
