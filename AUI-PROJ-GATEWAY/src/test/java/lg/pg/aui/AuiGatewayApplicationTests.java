4020f8c354c5114039516c4584fbbfd33fbec299
varrios <lucjan.gackowski@gmail.com>
2024-11-13 23:04:12 +0100

    Gateway implemented

---
+++ AUI-PROJ-GATEWAY/src/test/java/lg/pg/aui/AuiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package lg.pg.aui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
